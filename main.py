f2eab1882313d46e7a597e4dce4742e26e9bb2d4
MultiTasking2011 <multitasking010@gmail.com>
2024-04-14 15:13:14 +0530

    class one

--- main.py
+++ main.py
@@ -1,3 +1,62 @@
-import turtle
+# Data Structures
+# 1. LIST->[]
 
-#halogit 
+# a = [1,2,3,4,'a','defghi']
+
+
+# if b in a:
+#     print()
+
+# print(a[2::4])
+
+# for i in a:
+#     print(i, end="\t")
+
+# for i in range(len(a)):
+#     print(i, a[i])
+
+# for i, j in enumerate(a):
+#     print(i, j)
+
+# Adding items
+# a.append("abcdef")
+# a.insert(0, "item_1")
+
+# deleting items
+# a.pop(2)
+# a.remove("defghi")
+
+# print(a)
+
+#2. Disctionaries -> {k:v}
+
+
+a = {"name": "Suraj", "age": 111}
+
+# print(a["name1"]) #indexing with a key name
+
+# print(a.get("name"))
+
+b = dict()
+
+b["gender"] = []
+b["gender"].append("Male")
+b["gender"].append("Female")
+
+
+# print(b)
+
+# .keys(), .values(), .items()
+
+for i,j in a.items():
+    print(i,j)
+
+#3. Sets -> {}
+
+c = {1,7,3,5,6,7,3}
+
+print(c)
+
+
+
+#4. Tuples -> ()
