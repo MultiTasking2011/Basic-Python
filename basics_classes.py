e6737736a97dec59e91f380ec5d7d38f935b8258
MultiTasking2011 <multitasking010@gmail.com>
2024-04-21 15:41:03 +0530

    i have added the python file

---
+++ basics_classes.py
@@ -0,0 +1,31 @@
+class Car:
+    def __init__(self,a,b) -> None:
+        self.a = a
+        self.b = b
+
+    # method
+    def tire(self):
+        return self.a
+    
+    def tire2(self):
+        self.b
+
+# OOP's -> Object Oriented Programmings
+bmw = Car("MRF","CEAT")
+toyota = Car("XYZ","ABC")
+
+print(bmw.a)
+print(toyota.a)
+
+# def test(b:float,c:float) -> float:
+#     return b+c
+
+
+
+# a = test(1,3)#arguments
+# print(a)
+ 
+# test(1,2)
+
+#test()
+
