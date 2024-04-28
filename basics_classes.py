69d627e07671e8b00041c8c736b3ca605bec7a6b
MultiTasking2011 <multitasking010@gmail.com>
2024-04-28 22:06:17 +0530

    Classes

--- basics_classes.py
+++ basics_classes.py
@@ -29,3 +29,75 @@ print(toyota.a)
 
 #test()
 
+# Inheritance: class A inherits class B's props
+# Parent class
+class Person:
+    def __init__(self, fname, lname) -> None:
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        return self.firstname, self.lastname
+
+# a = Person("suman", "banik")
+# a.printname()
+
+# Child class   
+
+# class Student(Person):
+#     def __init__(self, grade, rollno, fname, lname) -> None:
+#         # Person.__init__(fname, lname)
+#         super().__init__(fname, lname)
+#         self.grade = "A"
+#         self.rollno = 10
+        
+
+# x = Student("A", 100, "abc", "xyz")
+# abc = set(x)
+
+# x.printname()
+
+
+# Iterators
+# Iterable and Iterators.
+# List, tuples, disctionaries, sets are all iterable containers.
+
+a = ("Apple", "Banana", "Cherry")
+b = "Apple"
+c = list(a)
+d = {"x": 1, "y": 2, "z": 3, "s": 4}
+
+# for i in a: # a/b/c/d
+#     print(a) # it will itterate because of the iterable function hidden in it.
+
+# z = iter("sun")
+# print(next(z))
+# print(next(z))
+# print(next(z))
+
+class Numbers:
+    def __iter__(self) -> int:
+        self.a = 1
+        return self
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+    def __str__(self) -> str:
+        return "This is a iter class!!!"
+
+g = Numbers()
+
+h = iter(g)
+
+print(h)
+
+
+# print(next(h))
+# print(next(h))
+# print(next(h))
+# print(next(h))
+# print(next(h))
+
+
+# Polymorphism
